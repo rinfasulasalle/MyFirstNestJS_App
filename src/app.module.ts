68917374ec8ae3dfb6f7e8a97e41e7c599ce6412
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-05-04 22:49:20 -0500

    Genero mas modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.module';
+import { WorkerModule } from './worker/worker.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, WorkerModule, AuthModule],
   controllers: [],
   providers: [],
 })
