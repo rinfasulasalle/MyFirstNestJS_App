66a0d581d6eaff2081a765567c05f1352de237d0
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-05-05 00:51:26 -0500

    Controlador helo creado maualmente

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { WorkersModule } from './workers/workers.module';
 import { AuthModule } from './auth/auth.module';
+import { ActionController } from './action/action.controller';
 @Module({
   imports: [UsersModule, WorkersModule, AuthModule],
-  controllers: [],
+  controllers: [ActionController],
   providers: [],
 })
 export class AppModule {}
